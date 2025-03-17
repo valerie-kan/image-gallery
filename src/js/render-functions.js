ad601aa55215dd8d8c4329994eb05cd659cb1398
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2025-03-17 14:21:46 +0200

    change readme and some css

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,14 +1,14 @@
 export function renderGalleryItem(imgDetails) {
-    return `
+  return `
     <li class='gallery-item'>
         <a class='gallery-link' href='${imgDetails.largeImageURL}'>
         <img class='gallery-img' src='${imgDetails.webformatURL}' alt='${imgDetails.tags}'>
-        <div class='img-info'>
         </a>
+        <div class='img-info'>
         <p><span>Likes</span> ${imgDetails.likes}</p>
         <p><span>Views</span> ${imgDetails.views}</p>
         <p><span>Comments</span> ${imgDetails.comments}</p>
         <p><span>Downloads</span> ${imgDetails.downloads}</p>
         </div>
-    </li>`
+    </li>`;
 }
